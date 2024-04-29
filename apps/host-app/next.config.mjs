c51f09f52e825625b39d23d89e668147c4859eb4
Arya Ganesh <arya241>
2024-04-29 16:57:25 +0530

    styles updated

--- apps/host-app/next.config.mjs
+++ apps/host-app/next.config.mjs
@@ -5,16 +5,12 @@ export default {
     if (!isServer) {
       config.plugins.push(
         new NextFederationPlugin({
-          name: 'mfe1', // Replace with a unique name
-          filename: 'remoteEntry.js',
+          name: 'host-app',
+          // filename: 'remoteEntry.js',
           remotes: {
-            app1: 'http://localhost:3001/remoteEntry.js',
-            app2: 'http://localhost:3002/remoteEntry.js',
+            app1: 'app1@http://localhost:3001/remoteEntry.js',
+            // app2: 'http://localhost:3002/remoteEntry.js',
           },
-          exposes: {
-            './MyContainerAppComponent': './components/./MyContainerAppComponent', // Replace with your component path
-            // Expose other components or modules as needed
-          }
         })
       );
     }
